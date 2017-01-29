46f76b2f6a101fc91bc2d60099b76a3f69bef0d4
Marko Bandzo <marko_bandzo@hotmail.com>
2017-01-30 11:10:34 +1300

    Add files via upload

--- vector1.cpp
+++ vector1.cpp
@@ -10,20 +10,53 @@
  */
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 
 int main()
 {
-    vector<int> coll;    // vector container for integer elements
+    // create empty vector for strings
+    vector<string> sentence;
 
-    // append elements with values 1 to 6
-    for (int i=1; i<=6; ++i) {
-        coll.push_back(i);
-    }
+    // reserve memory for five elements to avoid reallocation
+    sentence.reserve(5);
 
-    // print all elements followed by a space
-    for (int i=0; i<coll.size(); ++i) {
-        cout << coll[i] << ' ';
-    }
+    // append some elements
+    sentence.push_back("Hello,");
+    sentence.push_back("how");
+    sentence.push_back("are");
+    sentence.push_back("you");
+    sentence.push_back("?");
+
+    // print elements separated with spaces
+    copy (sentence.begin(), sentence.end(),
+          ostream_iterator<string>(cout," "));
     cout << endl;
+
+    // print ``technical data''
+    cout << "  max_size(): " << sentence.max_size() << endl;
+    cout << "  size():     " << sentence.size()     << endl;
+    cout << "  capacity(): " << sentence.capacity() << endl;
+
+    // swap second and fourth element
+    swap (sentence[1], sentence[3]);
+
+    // insert element "always" before element "?"
+    sentence.insert (find(sentence.begin(),sentence.end(),"?"),
+                     "always");
+
+    // assign "!" to the last element
+    sentence.back() = "!";
+    
+    // print elements separated with spaces
+    copy (sentence.begin(), sentence.end(),
+          ostream_iterator<string>(cout," "));
+    cout << endl;
+
+    // print ``technical data'' again
+    cout << "  max_size(): " << sentence.max_size() << endl;
+    cout << "  size():     " << sentence.size()     << endl;
+    cout << "  capacity(): " << sentence.capacity() << endl;
 }

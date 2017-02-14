250e3360625764f6f8cbf6074876f2588932fa07
Marko Bandzo <marko_bandzo@hotmail.com>
2017-02-14 17:22:56 +1300

    Add files via upload

--- foreach3.cpp
+++ foreach3.cpp
@@ -8,9 +8,7 @@
  * This software is provided "as is" without express or implied
  * warranty, and with no claim as to its suitability for any purpose.
  */
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include "algostuff.hpp"
 using namespace std;
 
 // function object to process the mean value
@@ -23,15 +21,15 @@ class MeanValue {
     MeanValue () : num(0), sum(0) {
     }
 
-    // ``function call''
+    // function call
     // - process one more element of the sequence
     void operator() (int elem) {
         num++;          // increment count
         sum += elem;    // add value
     }
 
-    // return mean value
-    double value () {
+    // return mean value (implicit type conversion)
+    operator double() {
         return static_cast<double>(sum) / static_cast<double>(num);
     }
 };
@@ -40,13 +38,10 @@ int main()
 {
     vector<int> coll;
 
-    // insert elments from 1 to 8
-    for (int i=1; i<=8; ++i) {
-        coll.push_back(i);
-    }
+    INSERT_ELEMENTS(coll,1,8);
 
     // process and print mean value
-    MeanValue mv = for_each (coll.begin(), coll.end(),  // range
-                             MeanValue());              // operation
-    cout << "mean value: " << mv.value() << endl;
+    double mv = for_each (coll.begin(), coll.end(),  // range
+                          MeanValue());              // operation
+    cout << "mean value: " << mv << endl;
 }

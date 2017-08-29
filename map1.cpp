0e8c117e12a1e961f88b9e8fff631dc467baed17
Marko Bandzo <marko_bandzo@hotmail.com>
2017-08-30 10:06:23 +1200

    Add files via upload

--- map1.cpp
+++ map1.cpp
@@ -15,29 +15,50 @@ using namespace std;
 
 int main()
 {
-    /* type of the container:
-     * - map: elements key/value pairs
-     * - string: keys have type string
-     * - float: values have type float
+    /* create map / associative array
+     * - keys are strings
+     * - values are floats
      */
     typedef map<string,float> StringFloatMap;
 
-    StringFloatMap coll;
+    StringFloatMap stocks;      // create empty container
 
-    // insert some elements into the collection
-    coll["VAT"] = 0.15;
-    coll["Pi"] = 3.1415;
-    coll["an arbitrary number"] = 4983.223;
-    coll["Null"] = 0;
+    // insert some elements
+    stocks["BASF"] = 369.50;
+    stocks["VW"] = 413.50;
+    stocks["Daimler"] = 819.00;
+    stocks["BMW"] = 834.00;
+    stocks["Siemens"] = 842.20;
 
-    /* print all elements
-     * - iterate over all elements
-     * - element member first is the key
-     * - element member second is the value
-     */
+    // print all elements
     StringFloatMap::iterator pos;
-    for (pos = coll.begin(); pos != coll.end(); ++pos) {
-        cout << "key: \"" << pos->first << "\" "
-             << "value: " << pos->second << endl;
+    for (pos = stocks.begin(); pos != stocks.end(); ++pos) {
+        cout << "stock: " << pos->first << "\t"
+             << "price: " << pos->second << endl;
+    }
+    cout << endl;
+
+    // boom (all prices doubled)
+    for (pos = stocks.begin(); pos != stocks.end(); ++pos) {
+        pos->second *= 2;
+    }
+
+    // print all elements
+    for (pos = stocks.begin(); pos != stocks.end(); ++pos) {
+        cout << "stock: " << pos->first << "\t"
+             << "price: " << pos->second << endl;
+    }
+    cout << endl;
+
+    /* rename key from "VW" to "Volkswagen"
+     * - only provided by exchanging element
+     */
+    stocks["Volkswagen"] = stocks["VW"];
+    stocks.erase("VW");
+
+    // print all elements
+    for (pos = stocks.begin(); pos != stocks.end(); ++pos) {
+        cout << "stock: " << pos->first << "\t"
+             << "price: " << pos->second << endl;
     }
 }
